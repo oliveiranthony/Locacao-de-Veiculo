71738c5f81251d2228144d09eb2836bbb2342b57
Anthony Oliveira Carvalho <anthonyol733@gmail.com>
2026-06-28 15:48:04 -0300

    Versão com a interface TaxService

--- src/model/services/BrazilTaxService.java
+++ src/model/services/BrazilTaxService.java
@@ -1,7 +1,7 @@
 package model.services;
 import model.entities.Invoice;
 
-public class BrazilTaxService {
+public class BrazilTaxService implements TaxService {
     public double tax(double amount) {
         if(amount <= 100.00) {
             return amount * 0.2;
